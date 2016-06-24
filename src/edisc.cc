3678275e88561abf32f55e83f2ad7a1cfabeee47
Tom Marsh <t.r.marsh@warwick.ac.uk>
2016-06-24 09:19:21 +0100

    fixed a bug spotted by Chris Manser in vellipse

--- src/edisc.cc
+++ src/edisc.cc
@@ -55,209 +55,209 @@ int main (int argc, char *argv[]){
 
     try{
 
-	// Construct Input object
+        // Construct Input object
 
-	Subs::Input input(argc, argv, Binary::BINARY_ENV, Binary::BINARY_DIR);
+        Subs::Input input(argc, argv, Binary::BINARY_ENV, Binary::BINARY_DIR);
 
-	// Define inputs
+        // Define inputs
 
-	input.sign_in("m",       Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("e",       Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("alpha",   Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("a1",      Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("a2",      Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("dr",      Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("beta",    Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("data",    Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("vmax",    Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("vstep",   Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("nsplit",  Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("fwhm",    Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("q",       Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("az",      Subs::Input::LOCAL, Subs::Input::PROMPT);
-	input.sign_in("plot",    Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("m",       Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("e",       Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("alpha",   Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("a1",      Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("a2",      Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("dr",      Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("beta",    Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("data",    Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("vmax",    Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("vstep",   Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("nsplit",  Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("fwhm",    Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("q",       Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("az",      Subs::Input::LOCAL, Subs::Input::PROMPT);
+        input.sign_in("plot",    Subs::Input::LOCAL, Subs::Input::PROMPT);
 
-	Subs::Format form(10);
+        Subs::Format form(10);
 
-	double m;
-	input.get_value("m",  m, 0.5, 1.e-5, 1.e5, "mass of central object (solar masses)");
-	double e;
-	input.get_value("e",  e, 0.5, 0., 0.99999999, "eccentricity of orbits");
-	double alpha;
-	input.get_value("alpha", alpha, 80., -10000., 10000., "argument of pericentre (degrees)");
-	double a1;
-	input.get_value("a1",  a1, 0.1, 1.e-5, 1.e5, "inner semi-major axis");
-	double a2;
-	input.get_value("a2",  a2, std::max(a1, 100.), a1, 1.e8, "outer semi-major axis");
-	double dr;
-	input.get_value("dr",  dr, 0.01, 1.e-4, 10., "radial width of annuli");
-	double beta;
-	input.get_value("beta", beta, -2., -100., 100., "emissivity power-law exponent");
-	std::string sdata;
-	double vstart, vstep;
-	int npix;
-	input.get_value("data", sdata, "null", "data file to store (km/s)");
-	Subs::Array1D<Subs::xyz<double,float,float> > data;
-	if(sdata == "none"){
-	    double vmax;
-	    input.get_value("vmax", vmax, 2000., 1., 100000., "maximum velocity to store (km/s)");
-	    input.get_value("vstep", vstep, 100., 1., 10000., "velocity step size (km/s)");
-	    npix   = int(2*vmax/vstep) + 1;
-	    vstart = -vstep*npix/2;
-	}else{
-	    data.load_ascii(sdata);
-	    vstart = data[0].x;
-	    npix   = data.size();
-	    double vend = data[npix-1].x;
-	    vstep = (vend-vstart)/(npix-1);
-	    double dmax = 0.;
-	    for(int i=0; i<npix; i++){
-		double dev = Subs::abs((data[i].x - (vstart + vstep*i))/vstep);
-		if(dev > dmax) dmax = dev;
-	    }
-	    std::cerr << "Maximum deviation from linearity = " << form(dmax) << " pixels" << std::endl;
-	    vstart -= vstep/2.;
-	    std::cerr << "Velocity step size = " << form(vstep) << " km/s, number of pixels = " << npix << std::endl;
-	    std::cerr << "Velocity range = " << form(vstart) << " to " << form(vstart + npix*vstep) << std::endl;
-	}
+        double m;
+        input.get_value("m",  m, 0.5, 1.e-5, 1.e5, "mass of central object (solar masses)");
+        double e;
+        input.get_value("e",  e, 0.5, 0., 0.99999999, "eccentricity of orbits");
+        double alpha;
+        input.get_value("alpha", alpha, 80., -10000., 10000., "argument of pericentre (degrees)");
+        double a1;
+        input.get_value("a1",  a1, 0.1, 1.e-5, 1.e5, "inner semi-major axis");
+        double a2;
+        input.get_value("a2",  a2, std::max(a1, 100.), a1, 1.e8, "outer semi-major axis");
+        double dr;
+        input.get_value("dr",  dr, 0.01, 1.e-4, 10., "radial width of annuli");
+        double beta;
+        input.get_value("beta", beta, -2., -100., 100., "emissivity power-law exponent");
+        std::string sdata;
+        double vstart, vstep;
+        int npix;
+        input.get_value("data", sdata, "null", "data file to store (km/s)");
+        Subs::Array1D<Subs::xyz<double,float,float> > data;
+        if(sdata == "none"){
+            double vmax;
+            input.get_value("vmax", vmax, 2000., 1., 100000., "maximum velocity to store (km/s)");
+            input.get_value("vstep", vstep, 100., 1., 10000., "velocity step size (km/s)");
+            npix   = int(2*vmax/vstep) + 1;
+            vstart = -vstep*npix/2;
+        }else{
+            data.load_ascii(sdata);
+            vstart = data[0].x;
+            npix   = data.size();
+            double vend = data[npix-1].x;
+            vstep = (vend-vstart)/(npix-1);
+            double dmax = 0.;
+            for(int i=0; i<npix; i++){
+                double dev = Subs::abs((data[i].x - (vstart + vstep*i))/vstep);
+                if(dev > dmax) dmax = dev;
+            }
+            std::cerr << "Maximum deviation from linearity = " << form(dmax) << " pixels" << std::endl;
+            vstart -= vstep/2.;
+            std::cerr << "Velocity step size = " << form(vstep) << " km/s, number of pixels = " << npix << std::endl;
+            std::cerr << "Velocity range = " << form(vstart) << " to " << form(vstart + npix*vstep) << std::endl;
+        }
 
-	int nsplit;
-	input.get_value("nsplit", nsplit, 5, 1, 1000, "sub-division factor");
-	const int NFINE = nsplit*npix;
+        int nsplit;
+        input.get_value("nsplit", nsplit, 5, 1, 1000, "sub-division factor");
+        const int NFINE = nsplit*npix;
       
-	double fwhm;
-	input.get_value("fwhm", fwhm, 200., 1., 10000., "FWHM resolution (km/s)");
-	double q;
-	input.get_value("q", q, 0., 0., 1000., "importance of shear broadening");
-	double az;
-	input.get_value("az", az, 0., -1., 1., "azimuthal asymmetry");
-	bool mplot;
-	input.get_value("plot", mplot, false, "do you want a plot?");
+        double fwhm;
+        input.get_value("fwhm", fwhm, 200., 1., 10000., "FWHM resolution (km/s)");
+        double q;
+        input.get_value("q", q, 0., 0., 1000., "importance of shear broadening");
+        double az;
+        input.get_value("az", az, 0., -1., 1., "azimuthal asymmetry");
+        bool mplot;
+        input.get_value("plot", mplot, false, "do you want a plot?");
 
-	Subs::Array1D<double> fprof(NFINE);
+        Subs::Array1D<double> fprof(NFINE);
 
-	fprof = 0.;
+        fprof = 0.;
 
-	const int NA = int((a2-a1)/dr) + 1;
-	Binary::Einfo einfo;
-	for(int n=0; n<NA; n++){
-	    double a    = a1 + (a2-a1)*(n+0.5)/NA;
-	    int ntheta = int(Constants::TWOPI*a/dr);
-	    ntheta = ntheta > 32 ? ntheta : 32;
-	    for(int nt=0; nt<ntheta; nt++){
+        const int NA = int((a2-a1)/dr) + 1;
+        Binary::Einfo einfo;
+        for(int n=0; n<NA; n++){
+            double a    = a1 + (a2-a1)*(n+0.5)/NA;
+            int ntheta = int(Constants::TWOPI*a/dr);
+            ntheta = ntheta > 32 ? ntheta : 32;
+            for(int nt=0; nt<ntheta; nt++){
 
-		double theta = Constants::TWOPI*nt/ntheta;
+                double theta = Constants::TWOPI*nt/ntheta;
 
-		// Compute RV information
-		einfo = Binary::vellipse(m, e, a, theta);
+                // Compute RV information
+                einfo = Binary::vellipse(m, e, a, theta);
 
-		double area  = Constants::TWOPI/ntheta*dr/a*Subs::sqr(einfo.r);
-		double emiss = area*pow(einfo.r,beta)*(1+az*cos(theta));
+                double area  = Constants::TWOPI/ntheta*dr/a*Subs::sqr(einfo.r);
+                double emiss = area*pow(einfo.r,beta)*(1+az*cos(theta));
 
-		double cosa  = cos(Constants::TWOPI*alpha/360);
-		double sina  = sin(Constants::TWOPI*alpha/360);
-		double rv    = einfo.vx*cosa + einfo.vy*sina;
-		double shear = (einfo.vx_x*cosa*cosa + (einfo.vx_y+einfo.vy_x)*cosa*sina + einfo.vy_y*sina*sina)/einfo.omega;
+                double cosa  = cos(Constants::TWOPI*alpha/360);
+                double sina  = sin(Constants::TWOPI*alpha/360);
+                double rv    = einfo.vx*cosa + einfo.vy*sina;
+                double shear = (einfo.vx_x*cosa*cosa + (einfo.vx_y+einfo.vy_x)*cosa*sina + einfo.vy_y*sina*sina)/einfo.omega;
 
-		int nadd = int(nsplit*(rv-vstart)/vstep);
+                int nadd = int(nsplit*(rv-vstart)/vstep);
 
-		if(nadd >=0 && nadd < NFINE) fprof[nadd] += sqrt(1+Subs::sqr(q*shear))*emiss;
-	    }
-	}
+                if(nadd >=0 && nadd < NFINE) fprof[nadd] += sqrt(1+Subs::sqr(q*shear))*emiss;
+            }
+        }
 
-	// create blurr array
-	const int NBLURR = int(4.*nsplit*fwhm/vstep + 1);
-	Subs::Array1D<double> blurr(2*NBLURR+1);
-	double norm = 0.;
-	for(int nb=0; nb<2*NBLURR+1; nb++){
-	    blurr[nb] = exp(-Subs::sqr(vstep*(nb-NBLURR)/nsplit/(fwhm/2.3548))/2.);
-	    norm += blurr[nb];
-	}
-	blurr /= norm;
+        // create blurr array
+        const int NBLURR = int(4.*nsplit*fwhm/vstep + 1);
+        Subs::Array1D<double> blurr(2*NBLURR+1);
+        double norm = 0.;
+        for(int nb=0; nb<2*NBLURR+1; nb++){
+            blurr[nb] = exp(-Subs::sqr(vstep*(nb-NBLURR)/nsplit/(fwhm/2.3548))/2.);
+            norm += blurr[nb];
+        }
+        blurr /= norm;
 
-	// blurr array
-	Subs::Array1D<double> tfprof(NFINE);
-	tfprof = 0.;
-	for(int nf=0; nf<NFINE; nf++){
-	    for(int nb=0, nadd=nf-NBLURR; nb<2*NBLURR+1; nb++, nadd++){
-		if(nadd >=0 && nadd < NFINE)
-		    tfprof[nadd] += blurr[nb]*fprof[nf];
-	    }
-	}
+        // blurr array
+        Subs::Array1D<double> tfprof(NFINE);
+        tfprof = 0.;
+        for(int nf=0; nf<NFINE; nf++){
+            for(int nb=0, nadd=nf-NBLURR; nb<2*NBLURR+1; nb++, nadd++){
+                if(nadd >=0 && nadd < NFINE)
+                    tfprof[nadd] += blurr[nb]*fprof[nf];
+            }
+        }
 
-	// Bin and print out
-	Subs::Array1D<float>  x(npix);
-	Subs::Array1D<double> y(npix);
-	y = 0.;
-	double sum1 = 0., sum2 = 0.;
-	for(int np=0; np<npix; np++){
-	    x[np] = vstart + vstep*(np+0.5);
-	    for(int nfp=nsplit*np; nfp<nsplit*(np+1); nfp++)
-		y[np] += tfprof[nfp];
+        // Bin and print out
+        Subs::Array1D<float>  x(npix);
+        Subs::Array1D<double> y(npix);
+        y = 0.;
+        double sum1 = 0., sum2 = 0.;
+        for(int np=0; np<npix; np++){
+            x[np] = vstart + vstep*(np+0.5);
+            for(int nfp=nsplit*np; nfp<nsplit*(np+1); nfp++)
+                y[np] += tfprof[nfp];
 
-	    y[np] /= nsplit;
+            y[np] /= nsplit;
 
-	    // derive optimum scaling factor
-	    if(sdata != "none"){
-		sum1 += data[np].y*y[np]/Subs::sqr(data[np].z);
-		sum2 += Subs::sqr(y[np]/data[np].z);
-	    }
+            // derive optimum scaling factor
+            if(sdata != "none"){
+                sum1 += data[np].y*y[np]/Subs::sqr(data[np].z);
+                sum2 += Subs::sqr(y[np]/data[np].z);
+            }
 
-	}
+        }
 
-	// apply scaling
-	if(sdata != "none") y *= sum1/sum2;
+        // apply scaling
+        if(sdata != "none") y *= sum1/sum2;
       
-	Subs::Array1D<double> dy(npix);
-	if(sdata != "none"){
-	    double chisq = 0.;
-	    for(int np=0; np<npix; np++){
-		dy[np] = data[np].y;
-		chisq += Subs::sqr((dy[np]-y[np])/data[np].z);
-	    }
-	    std::cerr << "Chisq = " << form(chisq) << std::endl;
-	}
+        Subs::Array1D<double> dy(npix);
+        if(sdata != "none"){
+            double chisq = 0.;
+            for(int np=0; np<npix; np++){
+                dy[np] = data[np].y;
+                chisq += Subs::sqr((dy[np]-y[np])/data[np].z);
+            }
+            std::cerr << "Chisq = " << form(chisq) << std::endl;
+        }
 
-	// plot
-	float ymax = 1.1*y.max();
-	if(sdata != "none") ymax = std::max(ymax, float(1.1*dy.max()));
-	if(mplot){
-	    Subs::Plot plot("/xs");
-	    cpgsch(1.5);
-	    cpgslw(3);
-	    cpgenv(vstart, vstart + npix*vstep, -0.1*ymax, ymax, 0, 0);
-	    cpglab("Velocity (km/s)", "Flux density", " " );
-	    if(sdata != "none"){
-		cpgsci(2);
-		for(int np=0; np<npix; np++){
-		    cpgmove(x[np], dy[np] - data[np].z);
-		    cpgdraw(x[np], dy[np] + data[np].z);
-		}
-		cpgsci(1);
-		pgpt(x, dy, 17);
-		cpgsls(4);
-		cpgsci(2);
-	    }
+        // plot
+        float ymax = 1.1*y.max();
+        if(sdata != "none") ymax = std::max(ymax, float(1.1*dy.max()));
+        if(mplot){
+            Subs::Plot plot("/xs");
+            cpgsch(1.5);
+            cpgslw(3);
+            cpgenv(vstart, vstart + npix*vstep, -0.1*ymax, ymax, 0, 0);
+            cpglab("Velocity (km/s)", "Flux density", " " );
+            if(sdata != "none"){
+                cpgsci(2);
+                for(int np=0; np<npix; np++){
+                    cpgmove(x[np], dy[np] - data[np].z);
+                    cpgdraw(x[np], dy[np] + data[np].z);
+                }
+                cpgsci(1);
+                pgpt(x, dy, 17);
+                cpgsls(4);
+                cpgsci(2);
+            }
 	    
-	    // plot model
-	    pgline(x, y);
-	}   
+            // plot model
+            pgline(x, y);
+        }   
  
-	// Output the fit.
-	for(int np=0; np<npix; np++)
-	    std::cout << form(x[np]) << " " << y[np] << std::endl; 
+        // Output the fit.
+        for(int np=0; np<npix; np++)
+            std::cout << form(x[np]) << " " << y[np] << std::endl; 
 	    
     }
 
     catch(const Binary::Binary_Error& err){
-	std::cerr << "Binary::Binary_Error exception:" << std::endl;
-	std::cerr << err << std::endl;
-	exit(EXIT_FAILURE);
+        std::cerr << "Binary::Binary_Error exception:" << std::endl;
+        std::cerr << err << std::endl;
+        exit(EXIT_FAILURE);
     }
     catch(const std::string& err){
-	std::cerr << "string exception:" << std::endl;
-	std::cerr << err << std::endl;
-	exit(EXIT_FAILURE);
+        std::cerr << "string exception:" << std::endl;
+        std::cerr << err << std::endl;
+        exit(EXIT_FAILURE);
     }
 }
 
--- src/vellipse.cc
+++ src/vellipse.cc
@@ -17,86 +17,86 @@
 
 Binary::Einfo Binary::vellipse(double m, double e, double a, double theta){
 
-  // Compute polar coords
-  double sint  = sin(theta);
-  double cost  = cos(theta);
+    // Compute polar coords
+    double sint  = sin(theta);
+    double cost  = cos(theta);
 
-  // semi latus rectum
-  double l = a*(1-e*e);
+    // semi latus rectum
+    double l = a*(1-e*e);
 
-  // useful number
-  double fac = 1 + e*cost;
+    // useful number
+    double fac = 1 + e*cost;
 
-  // orbital frequency
-  double omega = sqrt(Constants::G*Constants::MSUN*m/pow(Constants::RSUN*a,3));
+    // orbital frequency
+    double omega = sqrt(Constants::G*Constants::MSUN*m/pow(Constants::RSUN*a,3));
 
-  // eccentric anomaly
-  double cosE = (e + cost)/fac;
-  double E    = acos(cosE);
-  E = (sint >= 0) ? E : Constants::TWOPI - E;
-  double sinE = sin(E);
+    // eccentric anomaly
+    double cosE = (e + cost)/fac;
+    double E    = acos(cosE);
+    E = (sint >= 0) ? E : Constants::TWOPI - E;
+    double sinE = sin(E);
 
-  // rate of change of eccentric anom.
-  double Edot = omega/(1-e*cosE);
+    // rate of change of eccentric anom.
+    double Edot = omega/(1-e*cosE);
 
-  // rate of change of theta
-  double zeta = (1+e)/(1-e);
-  double cosE2 = cos(E/2);
-  double sinE2 = sin(E/2);
-  double fac0  = Subs::sqr(cosE2) + zeta*Subs::sqr(sinE2);
-  double thetadot = sqrt(zeta)*Edot/fac0;
+    // rate of change of theta
+    double zeta = (1+e)/(1-e);
+    double cosE2 = cos(E/2);
+    double sinE2 = sin(E/2);
+    double fac0  = Subs::sqr(cosE2) + zeta*Subs::sqr(sinE2);
+    double thetadot = sqrt(zeta)*Edot/fac0;
 
-  // velocity in radial direction
-  double vr = e*l*sint*thetadot/(fac*fac);
+    // velocity in radial direction
+    double vr = e*l*sint*thetadot/(fac*fac);
 
-  // theta velocity
-  double vt = l*thetadot/fac;
+    // theta velocity
+    double vt = l*thetadot/fac;
 
-  // x, y velocity in solar radii/sec
-  double vx = vr*cost-vt*sint;
-  double vy = vr*sint + vt*cost;
+    // x, y velocity in solar radii/sec
+    double vx = vr*cost-vt*sint;
+    double vy = vr*sint + vt*cost;
 
-  // Now for the partial derivatives. _x means 
-  // wrt x etc.
-    
-  double r       = l/fac;
-  double theta_x = -sint/r;
-  double theta_y = +cost/r;
-  double r_x     = cost;
-  double r_y     = sint;
-  double l_x     = r_x*fac - e*r*sint*theta_x;
-  double l_y     = r_y*fac - e*r*sint*theta_y;
-  double omega_x = -3*omega/2.*l_x/l;
-  double omega_y = -3*omega/2.*l_y/l;
-  double E_x, E_y;
-  if(sint == 0){
-    E_x = 0;
-    E_y = (1-e*e)/Subs::sqr(fac)*theta_y;
-  }else{
-    E_x = (1-e*e)/Subs::sqr(fac)*sint/sinE*theta_x;
-    E_y = (1-e*e)/Subs::sqr(fac)*sint/sinE*theta_y;
-  }
+    // Now for the partial derivatives. _x means
+    // wrt x etc.
 
-  double fac1   = 1 - e*cosE;
-  double Edot_x = (omega_x - e*omega*sinE*E_x/fac1)/fac1;
-  double Edot_y = (omega_y - e*omega*sinE*E_y/fac1)/fac1;
+    double r       = l/fac;
+    double theta_x = -sint/r;
+    double theta_y = +cost/r;
+    double r_x     = cost;
+    double r_y     = sint;
+    double l_x     = r_x*fac - e*r*sint*theta_x;
+    double l_y     = r_y*fac - e*r*sint*theta_y;
+    double omega_x = -3*omega/2.*l_x/l;
+    double omega_y = -3*omega/2.*l_y/l;
+    double E_x, E_y;
+    if(sint == 0){
+        E_x = 0.;
+        E_y = 0.;
+    }else{
+        E_x = (1-e*e)/Subs::sqr(fac)*sint/sinE*theta_x;
+        E_y = (1-e*e)/Subs::sqr(fac)*sint/sinE*theta_y;
+    }
 
-  double thetadot_x = sqrt(zeta)/fac0*(Edot_x-(zeta-1)*cosE*E_x*Edot/2./fac0);
-  double thetadot_y = sqrt(zeta)/fac0*(Edot_y-(zeta-1)*cosE*E_y*Edot/2./fac0);
+    double fac1   = 1 - e*cosE;
+    double Edot_x = (omega_x - e*omega*sinE*E_x/fac1)/fac1;
+    double Edot_y = (omega_y - e*omega*sinE*E_y/fac1)/fac1;
 
-  double vr_x = e/(fac*fac)*(l*sint*thetadot_x + l*cost*theta_x*thetadot + l_x*sint*thetadot + 2*e/fac*l*sint*sint*theta_x*thetadot);
-  double vr_y = e/(fac*fac)*(l*sint*thetadot_y + l*cost*theta_y*thetadot + l_y*sint*thetadot + 2*e/fac*l*sint*sint*theta_y*thetadot);
-  double vt_x = 1/fac*(l*thetadot_x + l_x*thetadot + e/fac*l*sint*theta_x*thetadot);
-  double vt_y = 1/fac*(l*thetadot_y + l_y*thetadot + e/fac*l*sint*theta_y*thetadot);
+    double thetadot_x = sqrt(zeta)/fac0*(Edot_x-(zeta-1)*cosE*E_x*Edot/2./fac0);
+    double thetadot_y = sqrt(zeta)/fac0*(Edot_y-(zeta-1)*cosE*E_y*Edot/2./fac0);
 
-  double vx_x = vr_x*cost - vt_x*sint - (vr*sint + vt*cost)*theta_x;
-  double vx_y = vr_y*cost - vt_y*sint - (vr*sint + vt*cost)*theta_y;
-  double vy_x = vr_x*sint + vt_x*cost + (vr*cost - vt*sint)*theta_x;
-  double vy_y = vr_y*sint + vt_y*cost + (vr*cost - vt*sint)*theta_y;
+    double vr_x = e/(fac*fac)*(l*sint*thetadot_x + l*cost*theta_x*thetadot + l_x*sint*thetadot + 2*e/fac*l*sint*sint*theta_x*thetadot);
+    double vr_y = e/(fac*fac)*(l*sint*thetadot_y + l*cost*theta_y*thetadot + l_y*sint*thetadot + 2*e/fac*l*sint*sint*theta_y*thetadot);
+    double vt_x = 1/fac*(l*thetadot_x + l_x*thetadot + e/fac*l*sint*theta_x*thetadot);
+    double vt_y = 1/fac*(l*thetadot_y + l_y*thetadot + e/fac*l*sint*theta_y*thetadot);
 
-  // Return data, converting velocities to km/s
-  Einfo temp = {1.e-3*Constants::RSUN*vx, 1.e-3*Constants::RSUN*vy, vx_x, vx_y, vy_x, vy_y, r, omega, E};
-  return temp;
+    double vx_x = vr_x*cost - vt_x*sint - (vr*sint + vt*cost)*theta_x;
+    double vx_y = vr_y*cost - vt_y*sint - (vr*sint + vt*cost)*theta_y;
+    double vy_x = vr_x*sint + vt_x*cost + (vr*cost - vt*sint)*theta_x;
+    double vy_y = vr_y*sint + vt_y*cost + (vr*cost - vt*sint)*theta_y;
+
+    // Return data, converting velocities to km/s
+    Einfo temp = {1.e-3*Constants::RSUN*vx, 1.e-3*Constants::RSUN*vy, vx_x, vx_y, vy_x, vy_y, r, omega, E};
+    return temp;
 }
 
 
